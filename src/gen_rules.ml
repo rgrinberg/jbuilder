63b913ccc7fab6bc69425e46e641c7d546ec3832
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-12-10 19:17:49 +0800

    lint_modules for libraries and exes

--- src/gen_rules.ml
+++ src/gen_rules.ml
@@ -351,6 +351,8 @@ module Gen(P : Params) = struct
                                  None)
     in
 
+    SC.PP.lint_modules sctx ~dir ~dep_kind ~modules ~scope
+      ~lint:lib.buildable.lint ~lib_name:(Some lib.name);
 
     Option.iter alias_module ~f:(fun m ->
       SC.add_rule sctx
@@ -611,6 +613,10 @@ module Gen(P : Params) = struct
         ~lib_name:None
         ~scope
     in
+
+    SC.PP.lint_modules sctx ~dir ~dep_kind ~modules ~scope
+      ~lint:exes.buildable.lint ~lib_name:None;
+
     let item = List.hd exes.names in
     let dep_graph =
       Ocamldep.rules sctx ~dir ~item ~modules ~alias_module:None
