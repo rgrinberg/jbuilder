c90c611ac5b33a75986a8f13b19c5bcc0f2bea20
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-12-20 18:02:47 +0700

    improve reasonlypp to also be a linter

--- test/blackbox-tests/test-cases/reason/pp/reasononlypp.ml
+++ test/blackbox-tests/test-cases/reason/pp/reasononlypp.ml
@@ -1,8 +1,26 @@
+let lint = ref false
+let fname = ref None
+let usage =
+  Printf.sprintf "%s [-lint] file" (Filename.basename Sys.executable_name)
+let anon s =
+  match !fname with
+  | None -> fname := Some s
+  | Some _ -> raise (Arg.Bad "file must only be given once")
 
 let () =
-  let fname = Sys.argv.(1) in
+  Arg.parse
+    ["-lint", Arg.Set lint, "lint instead of preprocessing"
+    ] anon usage;
+  let fname =
+    match !fname with
+    | None -> raise (Arg.Bad "file must be provided")
+    | Some f -> f in
+
   if Filename.check_suffix fname ".re"
   || Filename.check_suffix fname ".rei" then (
+    if !lint then (
+      exit 0
+    );
     let ch = open_in fname in
     let rec loop () =
       match input_line ch with
