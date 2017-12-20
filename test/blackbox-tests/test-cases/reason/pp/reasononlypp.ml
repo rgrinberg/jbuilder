ca96fa712bf0b500e8da491125999259f7ae318d
Rudi Grinberg <rudi.grinberg@gmail.com>
2017-12-20 20:41:59 +0700

    Make the reason only pp double as linter
    When ran with the -lint flag

--- test/blackbox-tests/test-cases/reason/pp/reasononlypp.ml
+++ test/blackbox-tests/test-cases/reason/pp/reasononlypp.ml
@@ -7,6 +7,15 @@ let anon s =
   | None -> fname := Some s
   | Some _ -> raise (Arg.Bad "file must only be given once")
 
+let is_ascii s =
+  try
+    for i=0 to String.length s - 1 do
+      if Char.code (s.[i]) > 127 then raise Exit
+    done;
+    true
+  with Exit ->
+    false
+
 let () =
   Arg.parse
     ["-lint", Arg.Set lint, "lint instead of preprocessing"
@@ -18,16 +27,22 @@ let () =
 
   if Filename.check_suffix fname ".re"
   || Filename.check_suffix fname ".rei" then (
-    if !lint then (
-      exit 0
-    );
     let ch = open_in fname in
     let rec loop () =
       match input_line ch with
       | exception End_of_file -> ()
-      | line -> print_endline line; loop () in
+      | line when is_ascii line ->
+        if not !lint then (
+          print_endline line
+        );
+        loop ()
+      | _ ->
+        Format.eprintf "%s isn't source code@.%!" fname;
+        exit 1
+    in
     loop ();
-    close_in ch
+    close_in ch;
+    exit 0
   ) else (
     Format.eprintf "%s is not a reason source@.%!" fname;
     exit 1
