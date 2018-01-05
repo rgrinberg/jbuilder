03d5aab3bd067beb09c18d4a0979affb48360c9e
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-01-05 20:06:30 +0800

    Add a documentation stanza
    for attaching mld files to packages

--- src/gen_rules.ml
+++ src/gen_rules.ml
@@ -235,7 +235,7 @@ module Gen(P : Install_rules.Params) = struct
                   match (dep : Jbuild.Lib_dep.t) with
                   | Direct _ -> None
                   | Select s -> Some s.result_fn)
-              | Alias _ | Provides _ | Install _ -> [])
+              | Alias _ | Provides _ | Install _ | Documentation _ -> [])
             |> String_set.of_list
           in
           String_set.union generated_files
