4914527fb09c6ed83f92522b52c99ec1012a372b
Jeremie Dimino <jdimino@janestreet.com>
2018-05-21 12:05:23 +0100

    Make sure sexp conversion functions in path.ml preserve the invariants

--- src/stdune/path.ml
+++ src/stdune/path.ml
@@ -38,8 +38,13 @@ end = struct
         [ "t", Sexp.To_sexp.string t ];
     t
 
-  let sexp_of_t = Sexp.To_sexp.string
-  let t = Sexp.Of_sexp.string
+  let sexp_of_t t = Sexp.To_sexp.string (to_string t)
+  let t sexp =
+    let t = Sexp.Of_sexp.string sexp in
+    if Filename.is_relative t then
+      Sexp.Of_sexp.of_sexp_error sexp "Absolute path expected";
+    t
+
 (*
   let rec cd_dot_dot t =
     match Unix.readlink t with
@@ -103,9 +108,6 @@ end = struct
   (* either "" for root, either a '/' separated list of components other that ".", ".."
      and not containing '/'. *)
   type t = string
-
-  let t = Sexp.Of_sexp.string
-  let sexp_of_t = Sexp.To_sexp.string
 
   let root = ""
 
@@ -224,6 +226,11 @@ end = struct
       s
     else
       relative "" s ?error_loc
+
+  let sexp_of_t t = Sexp.To_sexp.string (to_string t)
+  let t sexp =
+    of_string (Sexp.Of_sexp.string sexp)
+      ~error_loc:(Sexp.Ast.loc sexp)
 
   let rec mkdir_p = function
     | "" -> ()
