60a68fecfd58cb41995bf8038cc9f4b366a7c2af
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-10-26 14:08:46 -0400

    Set environment variables for rules executed by dune
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/preprocessing.ml
+++ src/preprocessing.ml
@@ -288,12 +288,13 @@ let build_ppx_driver sctx ~lib_db ~dep_kind ~target ~dir_kind pps =
   (* CR-someday diml: what we should do is build the .cmx/.cmo once
      and for all at the point where the driver is defined. *)
   let ml = Path.relative (Option.value_exn (Path.parent target)) "ppx.ml" in
-  SC.add_rule sctx
+  let add_rule = SC.add_rule sctx ~dir:(Super_context.build_dir sctx) in
+  add_rule
     (Build.of_result_map driver_and_libs ~f:(fun (driver, _) ->
        Build.return (sprintf "let () = %s ()\n" driver.info.main))
      >>>
      Build.write_file_dyn ml);
-  SC.add_rule sctx
+  add_rule
     (Build.record_lib_deps
        (Lib_deps.info ~kind:dep_kind (Lib_deps.of_pps pps))
      >>>
@@ -442,7 +443,7 @@ let setup_reason_rules sctx (m : Module.t) =
         Path.extend_basename base ~suffix
       in
       let ml = Module.File.make OCaml path in
-      SC.add_rule sctx (rule f.path ml.path);
+      SC.add_rule sctx ~dir:ctx.build_dir (rule f.path ml.path);
       ml)
 
 let promote_correction fn build ~suffix =
@@ -458,7 +459,7 @@ let lint_module sctx ~dir ~dep_kind ~lint ~lib_name ~scope ~dir_kind =
   Staged.stage (
     let alias = Build_system.Alias.lint ~dir in
     let add_alias fn build =
-      SC.add_alias_action sctx alias build
+      SC.add_alias_action sctx alias build ~dir
         ~stamp:("lint", lib_name, fn)
     in
     let lint =
@@ -552,7 +553,7 @@ let make sctx ~dir ~dep_kind ~lint ~preprocess
          let ast =
            pped_module m ~f:(fun _kind src dst ->
              let bindings = Pform.Map.input_file src in
-             SC.add_rule sctx ~loc
+             SC.add_rule sctx ~loc ~dir
                (preprocessor_deps
                 >>>
                 Build.path src
@@ -601,7 +602,7 @@ let make sctx ~dir ~dep_kind ~lint ~preprocess
            let ast = setup_reason_rules sctx m in
            if lint then lint_module ~ast ~source:m;
            pped_module ast ~f:(fun kind src dst ->
-             SC.add_rule sctx ~loc
+             SC.add_rule sctx ~loc ~dir
                (promote_correction ~suffix:corrected_suffix
                   (Option.value_exn (Module.file m kind))
                   (preprocessor_deps >>^ ignore
