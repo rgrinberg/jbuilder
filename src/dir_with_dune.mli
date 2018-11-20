72176aa467c060dbd4d0df298e3dd1053b9a6266
Jérémie Dimino <jeremie@dimino.org>
2018-11-20 10:01:43 -0500

    Update src/dir_with_dune.mli
    Co-Authored-By: rgrinberg <rudi.grinberg@gmail.com>

--- src/dir_with_dune.mli
+++ src/dir_with_dune.mli
@@ -1,6 +1,6 @@
 open Stdune
 
-(** A directory with a jbuild *)
+(** A directory with a [dune] file *)
 type 'data t =
   { src_dir : Path.t
   ; ctx_dir : Path.t  (** [_build/context-name/src_dir] *)
