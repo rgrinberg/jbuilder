665f16f9501c0046de3d032e783d002511c08b18
Rudi Grinberg <rudi.grinberg@gmail.com>
2019-03-19 16:18:17 +0700

    [WIP] add alias to Dep.t
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- src/dep.ml
+++ src/dep.ml
@@ -1,10 +1,45 @@
 open Stdune
 
+module Alias = struct
+  type t =
+    { name : string
+    ; dir : Path.t
+    ; rec_ : bool
+    }
+
+  let to_dyn { name ; dir ; rec_ } =
+    let open Dyn in
+    Record
+      [ "name", String name
+      ; "dir", Path.to_dyn dir
+      ; "rec_", Bool rec_
+      ]
+
+  let pp fmt t = Dyn.pp fmt (to_dyn t)
+
+  let encode {name ; dir; rec_ } =
+    let open Dune_lang.Encoder in
+    record
+      [ "name", string name
+      ; "dir", Path_dune_lang.encode dir
+      ; "rec_", bool rec_
+      ]
+
+  let compare x y =
+    match String.compare x.name y.name with
+    | Gt | Lt as x -> x
+    | Eq ->
+      match Path.compare x.dir y.dir with
+      | Gt | Lt as x -> x
+      | Eq -> Bool.compare x.rec_ y.rec_
+end
+
 module T = struct
   type t =
     | Env of Env.Var.t
     | File of Path.t
     | Universe
+    | Alias of Alias.t
 
   let env e = Env e
   let file f = File f
@@ -13,21 +48,24 @@ module T = struct
   let compare x y =
     match x, y with
     | Env x, Env y -> Env.Var.compare x y
-    | Env _, File _
-    | Env _, Universe -> Ordering.Lt
+    | Env _, (File _ | Universe | Alias _) -> Ordering.Lt
     | File x, File y -> Path.compare x y
     | File _, Env _ -> Ordering.Gt
-    | File _, Universe -> Ordering.Lt
+    | File _, (Alias _ | Universe) -> Lt
     | Universe, Universe -> Ordering.Eq
+    | Universe, Alias _ -> Lt
     | Universe, (Env _ | File _) -> Gt
+    | Alias x, Alias y -> Alias.compare x y
+    | Alias _, (Env _ | File _ | Universe) -> Gt
 
   let unset = lazy (Digest.string "unset")
 
-  let trace t ~env =
+  let trace_path p = (Path.to_string p, Utils.Cached_digest.file p)
+
+  let trace t ~env ~stamps =
     match t with
-    | Universe -> ("universe", Digest.string "universe")
-    | File fn ->
-      (Path.to_string fn, Utils.Cached_digest.file fn)
+    | Universe -> ["universe", Digest.string "universe"]
+    | File p -> [trace_path p]
     | Env var ->
       let value =
         begin match Env.get env var with
@@ -35,12 +73,17 @@ module T = struct
         | Some v -> Digest.string v
         end
       in
-      (var, value)
+      [var, value]
+    | Alias a ->
+      stamps a
+      |> Path.Set.to_list
+      |> List.map ~f:trace_path
 
   let pp fmt = function
     | Env e -> Format.fprintf fmt "Env %S" e
     | File f -> Format.fprintf fmt "File %a" Path.pp f
     | Universe -> Format.fprintf fmt "Universe"
+    | Alias a -> Format.fprintf fmt "Alias %a" Alias.pp a
 
   let encode t =
     let open Dune_lang.Encoder in
@@ -48,6 +91,7 @@ module T = struct
     | Env e -> pair string string ("Env", e)
     | File f -> pair string Path_dune_lang.encode ("File", f)
     | Universe -> string "Universe"
+    | Alias a -> pair string Alias.encode ("Alias", a)
 end
 
 include T
@@ -67,21 +111,23 @@ module Set = struct
 
   let encode t = Dune_lang.Encoder.list encode (to_list t)
 
-  let file_list t =
+  let file_list t ~stamps =
     to_list t
-    |> List.filter_map ~f:(function
-      | File f -> Some f
+    |> List.concat_map ~f:(function
+      | Alias a -> Path.Set.to_list (stamps a)
+      | File f -> [f]
       | Universe
-      | Env _ -> None)
+      | Env _ -> [])
 
-  let paths t = Path.Set.of_list (file_list t)
+  let paths t ~stamps = Path.Set.of_list (file_list t ~stamps)
 
-  let parallel_iter t ~f = Fiber.parallel_iter ~f (file_list t)
+  let parallel_iter t ~f ~stamps = Fiber.parallel_iter ~f (file_list t ~stamps)
 
   let dirs t =
     fold t ~init:Path.Set.empty ~f:(fun f acc ->
       match f with
       | File f -> Path.Set.add acc (Path.parent_exn f)
+      | Alias { Alias. dir; _ } -> Path.Set.add acc dir
       | Universe
       | Env _ -> acc)
 end
--- src/dep.mli
+++ src/dep.mli
@@ -2,9 +2,18 @@ open Stdune
 
 type t
 
+module Alias : sig
+  type t =
+    { name : string
+    ; dir : Path.t
+    ; rec_ : bool
+    }
+end
+
 val file : Path.t -> t
 val env : Env.Var.t -> t
 val universe : t
+val alias : Alias.t -> t
 
 val compare : t -> t -> Ordering.t
 
@@ -19,11 +28,20 @@ module Set : sig
 
   val encode : t -> Dune_lang.t
 
-  val trace : t -> env:Env.t -> (string * Digest.t) list
+  val trace
+    : t
+    -> env:Env.t
+    -> stamps:(Alias.t -> Path.Set.t)
+    -> (string * Digest.t) list
 
   val add_paths : t -> Path.Set.t -> t
 
-  val parallel_iter : t -> f:(Path.t -> unit Fiber.t) -> unit Fiber.t
+  val parallel_iter
+    :  t
+    -> f:(Path.t -> unit Fiber.t)
+    -> stamps:(Alias.t -> Path.Set.t)
+    -> unit Fiber.t
+
   val pp : t Fmt.t
 
   val dirs : t -> Path.Set.t
