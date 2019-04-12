d08702b836afa7c847c7846b34a90fbdf0d59cb2
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-04-12 15:27:16 +0200

    [coq] Rename `(coqlib ...)` to `(coq.theory ...)`
    Support for the first form will be dropped in the 1.0 version of the
    Coq language.
    
    Signed-off-by: Emilio Jesus Gallego Arias <e+git@x80.org>

--- src/dune_file.ml
+++ src/dune_file.ml
@@ -1886,11 +1886,16 @@ module Coq = struct
 
   let unit_to_sexp () = Sexp.List []
 
-  let coqlib_p = "coqlib", decode >>| fun x -> [T x]
+  let coqlib_warn x =
+    Errors.warn x.loc "(coqlib ...) is deprecated, please use (coq.theory ...) instead";
+    x
+
+  let coqlib_p = "coqlib", decode >>| fun x -> [T (coqlib_warn x)]
+  let coqtheory_p = "coq.theory", decode >>| fun x -> [T x]
   let coqpp_p = "coq.pp", Coqpp.(decode >>| fun x -> [T x])
 
   let unit_stanzas =
-    let+ r = return [coqlib_p; coqpp_p] in
+    let+ r = return [coqlib_p; coqtheory_p; coqpp_p] in
     ((), r)
 
   let key =
