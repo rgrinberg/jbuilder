a81b3195e7663136a5d8021bc2bc0295c9bcd9b7
Rudi Grinberg <me@rgrinberg.com>
2026-05-29 19:39:20 +0000

    test: construct thread-safe channel with local queue
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- test/expect-tests/thread_safe_channel_tests.ml
+++ test/expect-tests/thread_safe_channel_tests.ml
@@ -1,46 +1,36 @@
 open Stdune
 include Dune_scheduler
 open Dune_tests_common
-open Dune_engine
 open Fiber.O
-module Thread_safe_channel = Dune_scheduler.For_tests.Thread_safe_channel
+module Thread_safe_channel = Dune_scheduler.Thread_safe_channel
 
 let () = init ()
 
-let config =
-  Clflags.display := Short;
-  { Scheduler.Config.concurrency = 1
-  ; print_ctrl_c_warning = false
-  ; watch_exclusions = []
-  }
-;;
-
-let go ?(timeout = Time.Span.of_secs 0.3) f =
-  try
-    Scheduler.Run.go ~timeout config ~file_watcher:No_watcher ~on_event:(fun _ -> ()) f
-  with
-  | Shutdown.E Requested -> ()
-;;
-
 let%expect_test "wakes fibers from side threads" =
-  go (fun () ->
-    let channel = Thread_safe_channel.create_for_current_scheduler () in
-    ignore
-      (Scheduler.spawn_thread ~name:"channel-test-producer" (fun () ->
-         for i = 1 to 3 do
-           match Thread_safe_channel.write channel i with
-           | `Ok -> ()
-           | `Closed -> print_endline "channel closed"
-         done;
-         Thread_safe_channel.close channel));
-    let rec read_all acc =
-      let* value = Thread_safe_channel.read channel in
-      match value with
-      | None -> Fiber.return (List.rev acc)
-      | Some value -> read_all (value :: acc)
-    in
-    let* values = read_all [] in
-    print_dyn (Dyn.list Dyn.int values);
-    Fiber.return ());
+  let event_queue = Event.Queue.create () in
+  let channel = Thread_safe_channel.create event_queue in
+  ignore
+    (Thread.create
+       (fun () ->
+          for i = 1 to 3 do
+            match Thread_safe_channel.write channel i with
+            | `Ok -> ()
+            | `Closed -> print_endline "channel closed"
+          done;
+          Thread_safe_channel.close channel)
+       ());
+  let rec read_all acc =
+    let* value = Thread_safe_channel.read channel in
+    match value with
+    | None -> Fiber.return (List.rev acc)
+    | Some value -> read_all (value :: acc)
+  in
+  let iter () =
+    match Event.Queue.next event_queue with
+    | Fiber_fill_ivar fill -> [ fill ]
+    | Shutdown _ | Job_complete_ready -> assert false
+  in
+  let values = Fiber.run (read_all []) ~iter in
+  print_dyn (Dyn.list Dyn.int values);
   [%expect {| [ 1; 2; 3 ] |}]
 ;;
